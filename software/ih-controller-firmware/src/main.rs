f40a14b11ca4efd8b995ab8344a771f6d12b0af0
Dion Dokter <diondokter@gmail.com>
2023-08-13 18:40:09 +0200

    Built out more of the modbus

--- software/ih-controller-firmware/src/main.rs
+++ software/ih-controller-firmware/src/main.rs
@@ -2,11 +2,14 @@
 #![no_main]
 #![feature(type_alias_impl_trait)]
 
+use core::pin::pin;
+
 use embassy_executor::Spawner;
+use embassy_futures::select::{select4, Either4};
 use embassy_stm32::{
     bind_interrupts,
     gpio::{Level, OutputOpenDrain, OutputType, Pull, Speed},
-    peripherals::{self, USART1},
+    peripherals::{self, TIM1, USART1},
     rcc::{ClockSrc, PllConfig},
     time::khz,
     timer::{
@@ -15,13 +18,11 @@ use embassy_stm32::{
     },
     usart::{self, Uart},
 };
-use embassy_sync::{blocking_mutex::raw::ThreadModeRawMutex, mutex::Mutex};
+use futures::StreamExt;
 use modbus::modbus_server;
-use rmodbus::server::context::ModbusContext;
-use static_cell::make_static;
 use {defmt_rtt as _, panic_probe as _};
 
-mod modbus;
+pub mod modbus;
 
 bind_interrupts!(struct Irqs {
     USART1 => usart::InterruptHandler<peripherals::USART1>;
@@ -52,12 +53,6 @@ async fn run(spawner: Spawner) {
     let rs485 = Uart::new_with_de(
         p.USART1, p.PB7, p.PA9, Irqs, p.PA12, p.DMA1_CH1, p.DMA1_CH2, config,
     );
-    let modbus_context = make_static!(Mutex::<ThreadModeRawMutex, _>::new(ModbusContext::<
-        0,
-        2,
-        8,
-        8,
-    >::new()));
 
     let driver_pwm = ComplementaryPwm::new(
         p.TIM1,
@@ -76,5 +71,32 @@ async fn run(spawner: Spawner) {
     let fan_tacho = p.PB9; // CH1
                            // TODO input capture for fan tacho
 
-    spawner.must_spawn(modbus_server(0, rs485, modbus_context));
+    spawner.must_spawn(modbus_server(0, rs485));
+}
+
+#[embassy_executor::task]
+async fn driver(mut driver_pwm: ComplementaryPwm<'static, TIM1>) -> ! {
+    let mut enable_listener = pin!(modbus::ENABLE.get_listener().await);
+    let mut frequency_listener = pin!(modbus::DRIVE_FREQUENCY.get_listener().await);
+    let mut measure_frequency_listener = pin!(modbus::COIL_MEASURE_FREQUENCY.get_listener().await);
+    let mut dutycycle_listener = pin!(modbus::COIL_POWER_DUTYCYCLE.get_listener().await);
+
+    driver_pwm.set_dead_time(0);
+
+    loop {
+        let result = select4(
+            enable_listener.next(),
+            frequency_listener.next(),
+            measure_frequency_listener.next(),
+            dutycycle_listener.next(),
+        )
+        .await;
+
+        match result {
+            Either4::First(enable) => todo!(),
+            Either4::Second(_) => todo!(),
+            Either4::Third(_) => todo!(),
+            Either4::Fourth(_) => todo!(),
+        }
+    }
 }
--- software/ih-controller-firmware/src/modbus.rs
+++ software/ih-controller-firmware/src/modbus.rs
@@ -1,17 +1,33 @@
+use core::future::poll_fn;
+use core::marker::PhantomData;
+use core::ops::RangeInclusive;
+use core::task::{Context, Poll, Waker};
+
 use super::USART1;
+use defmt::unwrap;
 use embassy_stm32::peripherals::{DMA1_CH1, DMA1_CH2};
 use embassy_stm32::usart::Uart;
 use embassy_sync::blocking_mutex::raw::ThreadModeRawMutex;
 use embassy_sync::mutex::Mutex;
+use futures::{Stream, StreamExt};
+use rmodbus::consts::*;
 use rmodbus::server::context::ModbusContext;
 use rmodbus::server::ModbusFrame;
 use rmodbus::ModbusFrameBuf;
+
+static MODBUS_CONTEXT: Mutex<ThreadModeRawMutex, ModbusContext<0, 2, 8, 8>> =
+    Mutex::<ThreadModeRawMutex, _>::new(ModbusContext::<0, 2, 8, 8>::new());
+
+static MODBUS_LISTENER_REGISTRATIONS: Mutex<
+    ThreadModeRawMutex,
+    [Option<ModbusListenerRegistration>; 8],
+> = Mutex::new([REGINIT; 8]);
+const REGINIT: Option<ModbusListenerRegistration> = None;
 
 #[embassy_executor::task]
 pub(crate) async fn modbus_server(
     unit_id: u8,
     mut rs485: Uart<'static, USART1, DMA1_CH1, DMA1_CH2>,
-    modbus_context: &'static Mutex<ThreadModeRawMutex, ModbusContext<0, 2, 8, 8>>,
 ) -> ! {
     let mut receive_buffer: ModbusFrameBuf = [0; 256];
     let mut response_buffer = heapless::Vec::<_, 256>::new();
@@ -38,12 +54,29 @@ pub(crate) async fn modbus_server(
 
         if frame.processing_required {
             let result = match frame.readonly {
-                true => frame.process_read(&*modbus_context.lock().await),
-                false => frame.process_write(&mut *modbus_context.lock().await),
+                true => frame.process_read(&*MODBUS_CONTEXT.lock().await),
+                false => frame.process_write(&mut *MODBUS_CONTEXT.lock().await),
             };
             if let Err(e) = result {
                 defmt::error!("Could not process modbus frame: {}", e as u8);
                 continue;
+            }
+
+            let address_range = frame.reg..=frame.reg + frame.count;
+            let address_type = match frame.func {
+                MODBUS_GET_COILS | MODBUS_SET_COIL | MODBUS_SET_COILS_BULK => AddressType::Coil,
+                MODBUS_GET_DISCRETES => AddressType::Discrete,
+                MODBUS_GET_HOLDINGS | MODBUS_SET_HOLDING | MODBUS_SET_HOLDINGS_BULK => {
+                    AddressType::Holding
+                }
+                MODBUS_GET_INPUTS => AddressType::Input,
+                _ => {
+                    defmt::unreachable!()
+                }
+            };
+
+            if !frame.readonly {
+                trigger_registrations(address_range, address_type).await;
             }
         }
         if frame.response_required {
@@ -60,3 +93,370 @@ pub(crate) async fn modbus_server(
         }
     }
 }
+
+async fn trigger_registrations(address_range: RangeInclusive<u16>, address_type: AddressType) {
+    MODBUS_LISTENER_REGISTRATIONS
+        .lock()
+        .await
+        .iter_mut()
+        .flatten()
+        .for_each(|registration| {
+            registration.on_context_written(address_range.clone(), address_type)
+        });
+}
+
+struct ModbusListenerRegistration {
+    address_range: RangeInclusive<u16>,
+    address_type: AddressType,
+    waker: Option<Waker>,
+}
+
+impl ModbusListenerRegistration {
+    fn new(
+        address_range: RangeInclusive<u16>,
+        address_type: AddressType,
+        waker: Option<Waker>,
+    ) -> Self {
+        Self {
+            address_range,
+            address_type,
+            waker,
+        }
+    }
+
+    fn on_context_written(
+        &mut self,
+        address_range: RangeInclusive<u16>,
+        address_type: AddressType,
+    ) {
+        if self.address_type == address_type
+            && ranges_overlap(address_range, self.address_range.clone())
+        {
+            if let Some(waker) = self.waker.take() {
+                waker.wake();
+            }
+        }
+    }
+}
+
+struct ModbusListener {
+    index: usize,
+}
+
+impl ModbusListener {
+    async fn new(address_range: RangeInclusive<u16>, address_type: AddressType) -> Self {
+        let waker = poll_fn(|cx| Poll::Ready(cx.waker().clone())).await;
+        let mut registrations = MODBUS_LISTENER_REGISTRATIONS.lock().await;
+
+        let index = unwrap!(registrations
+            .iter()
+            .enumerate()
+            .find(|(_, r)| r.is_none())
+            .map(|(i, _)| i));
+
+        registrations[index] = Some(ModbusListenerRegistration::new(
+            address_range,
+            address_type,
+            Some(waker),
+        ));
+
+        Self { index }
+    }
+
+    fn associate_bool<A: BitAddress + 'static>(
+        self,
+        register: &ModbusRegister<bool, A>,
+    ) -> impl Stream<Item = bool> + '_ {
+        self.then(|_| async { register.read().await })
+    }
+
+    fn associate_u16<A: RegisterAddress + 'static>(
+        self,
+        register: &ModbusRegister<u16, A>,
+    ) -> impl Stream<Item = u16> + '_ {
+        self.then(|_| async { register.read().await })
+    }
+
+    fn associate_u32<A: RegisterAddress + 'static>(
+        self,
+        register: &ModbusRegister<u32, A>,
+    ) -> impl Stream<Item = u32> + '_ {
+        self.then(|_| async { register.read().await })
+    }
+
+    fn associate_f32<A: RegisterAddress + 'static>(
+        self,
+        register: &ModbusRegister<f32, A>,
+    ) -> impl Stream<Item = f32> + '_ {
+        self.then(|_| async { register.read().await })
+    }
+}
+
+impl futures::Stream for ModbusListener {
+    type Item = ();
+
+    fn poll_next(
+        self: core::pin::Pin<&mut Self>,
+        cx: &mut Context<'_>,
+    ) -> Poll<Option<Self::Item>> {
+        // Should work because this is a private threadmode mutex
+        let mut registrations = unwrap!(MODBUS_LISTENER_REGISTRATIONS.try_lock());
+
+        let waker = &mut unwrap!(registrations[self.index].as_mut()).waker;
+
+        if waker.is_none() {
+            *waker = Some(cx.waker().clone());
+            Poll::Ready(Some(()))
+        } else {
+            Poll::Pending
+        }
+    }
+}
+
+impl Drop for ModbusListener {
+    fn drop(&mut self) {
+        // Should work because this is a private threadmode mutex
+        unwrap!(MODBUS_LISTENER_REGISTRATIONS.try_lock())[self.index] = None;
+    }
+}
+
+#[derive(Clone, Copy, PartialEq, Eq)]
+pub enum AddressType {
+    Coil,
+    Discrete,
+    Input,
+    Holding,
+}
+
+fn ranges_overlap(x: RangeInclusive<u16>, y: RangeInclusive<u16>) -> bool {
+    x.start() <= y.end() && y.start() <= x.end()
+}
+
+// ----- Control (holdings) -----
+/// The amount of times per second the heater coil resonance frequency and voltage max is measured
+pub const COIL_MEASURE_FREQUENCY: ModbusRegister<f32, Holdings> = ModbusRegister::new(0);
+/// The dutycycle of the PWM that drives the coil. 0..=1
+pub const COIL_POWER_DUTYCYCLE: ModbusRegister<f32, Holdings> = ModbusRegister::new(2);
+// ----- Control (coils) -----
+pub const ENABLE: ModbusRegister<bool, Coils> = ModbusRegister::new(0);
+
+// ----- Report (inputs) -----
+pub const DRIVE_FREQUENCY: ModbusRegister<f32, Inputs> = ModbusRegister::new(0);
+pub const FAN_RPM: ModbusRegister<f32, Inputs> = ModbusRegister::new(2);
+pub const COIL_VOLTAGE_MAX: ModbusRegister<f32, Inputs> = ModbusRegister::new(4);
+// ----- Report (discretes) -----
+pub const LED_GREEN: ModbusRegister<bool, Discretes> = ModbusRegister::new(0);
+pub const LED_RED: ModbusRegister<bool, Discretes> = ModbusRegister::new(1);
+
+pub struct ModbusRegister<T, A> {
+    pub address_start: u16,
+    _phantom: PhantomData<(T, A)>,
+}
+
+impl<T, A> ModbusRegister<T, A> {
+    pub const fn new(address_start: u16) -> Self {
+        Self {
+            address_start,
+            _phantom: PhantomData,
+        }
+    }
+}
+
+impl<A: BitAddress> ModbusRegister<bool, A> {
+    pub async fn read(&self) -> bool {
+        match A::TYPE {
+            AddressType::Coil => unwrap!(MODBUS_CONTEXT
+                .lock()
+                .await
+                .get_coil(self.address_start)
+                .map_err(|e| e as u8)),
+            AddressType::Discrete => unwrap!(MODBUS_CONTEXT
+                .lock()
+                .await
+                .get_discrete(self.address_start)
+                .map_err(|e| e as u8)),
+            _ => defmt::unreachable!(),
+        }
+    }
+
+    pub async fn write(&self, value: bool) {
+        match A::TYPE {
+            AddressType::Coil => unwrap!(MODBUS_CONTEXT
+                .lock()
+                .await
+                .set_coil(self.address_start, value)
+                .map_err(|e| e as u8)),
+            AddressType::Discrete => unwrap!(MODBUS_CONTEXT
+                .lock()
+                .await
+                .set_discrete(self.address_start, value)
+                .map_err(|e| e as u8)),
+            _ => defmt::unreachable!(),
+        };
+
+        trigger_registrations(self.address_start..=self.address_start, A::TYPE).await;
+    }
+
+    pub async fn get_listener(&self) -> impl Stream<Item = bool> + '_ {
+        ModbusListener::new(self.address_start..=self.address_start, A::TYPE)
+            .await
+            .associate_bool::<A>(self)
+    }
+}
+
+impl<A: RegisterAddress> ModbusRegister<f32, A> {
+    pub async fn read(&self) -> f32 {
+        match A::TYPE {
+            AddressType::Input => unwrap!(MODBUS_CONTEXT
+                .lock()
+                .await
+                .get_inputs_as_f32(self.address_start)
+                .map_err(|e| e as u8)),
+            AddressType::Holding => unwrap!(MODBUS_CONTEXT
+                .lock()
+                .await
+                .get_holdings_as_f32(self.address_start)
+                .map_err(|e| e as u8)),
+            _ => defmt::unreachable!(),
+        }
+    }
+
+    pub async fn write(&self, value: f32) {
+        match A::TYPE {
+            AddressType::Input => unwrap!(MODBUS_CONTEXT
+                .lock()
+                .await
+                .set_inputs_from_f32(self.address_start, value)
+                .map_err(|e| e as u8)),
+            AddressType::Holding => unwrap!(MODBUS_CONTEXT
+                .lock()
+                .await
+                .set_holdings_from_f32(self.address_start, value)
+                .map_err(|e| e as u8)),
+            _ => defmt::unreachable!(),
+        };
+
+        trigger_registrations(self.address_start..=self.address_start + 1, A::TYPE).await;
+    }
+
+    pub async fn get_listener(&self) -> impl Stream<Item = f32> + '_ {
+        ModbusListener::new(self.address_start..=self.address_start + 1, A::TYPE)
+            .await
+            .associate_f32(self)
+    }
+}
+
+impl<A: RegisterAddress> ModbusRegister<u32, A> {
+    pub async fn read(&self) -> u32 {
+        match A::TYPE {
+            AddressType::Input => unwrap!(MODBUS_CONTEXT
+                .lock()
+                .await
+                .get_inputs_as_u32(self.address_start)
+                .map_err(|e| e as u8)),
+            AddressType::Holding => unwrap!(MODBUS_CONTEXT
+                .lock()
+                .await
+                .get_holdings_as_u32(self.address_start)
+                .map_err(|e| e as u8)),
+            _ => defmt::unreachable!(),
+        }
+    }
+
+    pub async fn write(&self, value: u32) {
+        match A::TYPE {
+            AddressType::Input => unwrap!(MODBUS_CONTEXT
+                .lock()
+                .await
+                .set_inputs_from_u32(self.address_start, value)
+                .map_err(|e| e as u8)),
+            AddressType::Holding => unwrap!(MODBUS_CONTEXT
+                .lock()
+                .await
+                .set_holdings_from_u32(self.address_start, value)
+                .map_err(|e| e as u8)),
+            _ => defmt::unreachable!(),
+        };
+
+        trigger_registrations(self.address_start..=self.address_start + 1, A::TYPE).await;
+    }
+
+    pub async fn get_listener(&self) -> impl Stream<Item = u32> + '_ {
+        ModbusListener::new(self.address_start..=self.address_start + 1, A::TYPE)
+            .await
+            .associate_u32(self)
+    }
+}
+
+impl<A: RegisterAddress> ModbusRegister<u16, A> {
+    pub async fn read(&self) -> u16 {
+        match A::TYPE {
+            AddressType::Input => unwrap!(MODBUS_CONTEXT
+                .lock()
+                .await
+                .get_input(self.address_start)
+                .map_err(|e| e as u8)),
+            AddressType::Holding => unwrap!(MODBUS_CONTEXT
+                .lock()
+                .await
+                .get_holding(self.address_start)
+                .map_err(|e| e as u8)),
+            _ => defmt::unreachable!(),
+        }
+    }
+
+    pub async fn write(&self, value: u16) {
+        match A::TYPE {
+            AddressType::Input => unwrap!(MODBUS_CONTEXT
+                .lock()
+                .await
+                .set_input(self.address_start, value)
+                .map_err(|e| e as u8)),
+            AddressType::Holding => unwrap!(MODBUS_CONTEXT
+                .lock()
+                .await
+                .set_holding(self.address_start, value)
+                .map_err(|e| e as u8)),
+            _ => defmt::unreachable!(),
+        };
+
+        trigger_registrations(self.address_start..=self.address_start, A::TYPE).await;
+    }
+
+    pub async fn get_listener(&self) -> impl Stream<Item = u16> + '_ {
+        ModbusListener::new(self.address_start..=self.address_start, A::TYPE)
+            .await
+            .associate_u16(self)
+    }
+}
+
+pub struct Coils;
+pub struct Discretes;
+pub struct Inputs;
+pub struct Holdings;
+
+pub trait Address: 'static {
+    const TYPE: AddressType;
+}
+pub trait RegisterAddress: Address {}
+pub trait BitAddress: Address {}
+
+impl Address for Coils {
+    const TYPE: AddressType = AddressType::Coil;
+}
+impl BitAddress for Coils {}
+
+impl Address for Discretes {
+    const TYPE: AddressType = AddressType::Discrete;
+}
+impl BitAddress for Discretes {}
+
+impl Address for Inputs {
+    const TYPE: AddressType = AddressType::Input;
+}
+impl RegisterAddress for Inputs {}
+
+impl Address for Holdings {
+    const TYPE: AddressType = AddressType::Holding;
+}
+impl RegisterAddress for Holdings {}
