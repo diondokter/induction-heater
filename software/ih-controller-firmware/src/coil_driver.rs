03041818c2114bc83744830e4765f1a8cc92816c
Dion Dokter <diondokter@gmail.com>
2024-02-13 14:26:20 +0100

    Remove the playing with dutycycle

--- software/ih-controller-firmware/src/coil_driver.rs
+++ software/ih-controller-firmware/src/coil_driver.rs
@@ -1,12 +1,11 @@
 use core::pin::pin;
 
-use embassy_futures::select::{select3, Either3};
+use embassy_futures::select::{select, Either};
 use embassy_stm32::{
     peripherals::TIM1,
     time::Hertz,
     timer::{simple_pwm::SimplePwm, Channel, OutputPolarity},
 };
-use embassy_time::Duration;
 use futures::StreamExt;
 
 use crate::modbus;
@@ -29,66 +28,15 @@ pub async fn coil_driver(mut driver_pwm: SimplePwm<'static, TIM1>) -> ! {
     modbus::COIL_DRIVE_FREQUENCY.write(40_000);
     modbus::COIL_POWER_DUTYCYCLE.write(0.5);
 
-    const UPDATE_TICK_DURATION: Duration = Duration::from_millis(10);
-    let mut update_ticker = embassy_time::Ticker::every(UPDATE_TICK_DURATION);
-
-    let mut current_duty_cycle = 0.0f32;
-    // The amount the dutycycle changes per second
-    const DUTY_CYCLE_DELTA: f32 = 0.2;
-
     loop {
-        let result = select3(
-            update_ticker.next(),
-            frequency_listener.next(),
-            enable_listener.next(),
-        )
-        .await;
+        let result = select(frequency_listener.next(), enable_listener.next()).await;
 
         match result {
-            Either3::First(_) => {
-                let target_dutycycle = if modbus::COIL_POWER_ENABLE.read() {
-                    modbus::COIL_POWER_DUTYCYCLE.read()
-                } else {
-                    0.0
-                };
-
-                if target_dutycycle > current_duty_cycle {
-                    let new_dutycycle = (current_duty_cycle
-                        + DUTY_CYCLE_DELTA
-                            * (UPDATE_TICK_DURATION.as_micros() as f32 / 1_000_000.0))
-                        .min(target_dutycycle);
-
-                    set_duty_cycle(&mut driver_pwm, new_dutycycle);
-
-                    if current_duty_cycle >= f32::EPSILON {
-                        driver_pwm.enable(Channel::Ch1);
-                        driver_pwm.enable(Channel::Ch2);
-                    }
-
-                    current_duty_cycle = new_dutycycle;
-                }
-
-                if target_dutycycle < current_duty_cycle {
-                    let new_dutycycle = (current_duty_cycle
-                        - DUTY_CYCLE_DELTA
-                            * (UPDATE_TICK_DURATION.as_micros() as f32 / 1_000_000.0))
-                        .max(target_dutycycle);
-
-                    set_duty_cycle(&mut driver_pwm, new_dutycycle);
-
-                    if new_dutycycle <= f32::EPSILON {
-                        driver_pwm.disable(Channel::Ch1);
-                        driver_pwm.disable(Channel::Ch2);
-                    }
-
-                    current_duty_cycle = new_dutycycle;
-                }
-            }
-            Either3::Second(Some(frequency)) => {
+            Either::First(Some(frequency)) => {
                 driver_pwm.set_freq(Hertz(frequency.clamp(10_000, 80_000)));
-                set_duty_cycle(&mut driver_pwm, current_duty_cycle);
+                set_duty_cycle(&mut driver_pwm, 0.5);
             }
-            Either3::Third(Some(enable)) => {
+            Either::Second(Some(enable)) => {
                 modbus::LED_GREEN.write(enable);
             }
             _ => defmt::unreachable!(),
